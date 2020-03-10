8ed89c72550b08b32e8cf6107a5d7ec7d091e950
isabeelafig <isabela.figueiradepaula@gmail.com>
2020-03-10 17:58:08 -0300

    Teste Aula LP3

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes =
+            {
+                "isabela figueira",
+                "daniel luiz de paula",
+                "alice figueira",
+                "felipe maia",
+            };
+
+            foreach (var elements in nomes)
+                Console.WriteLine($"O nome é: {nomes}");
+
+            Console.ReadKey();
+        }
+    }
+}
